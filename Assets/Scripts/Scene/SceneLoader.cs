a93cfed8ea970cfeb405ab1bc7f3b6e485657954
yolanda octaviane <73147428+YolandaOctaviane@users.noreply.github.com>
2022-08-10 20:01:22 +0700

    revisi bug

--- Assets/Scripts/Scene/SceneLoader.cs
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] public HighscoreData highScoreData;
+
+    // reset score jadi 0 (biar kehapus play sambil aktifin script ini, terus keluar non aktifin script ini )
+    public void Start()
+    {
+        //highScoreData.Save(0, "highscore"); 
+    }
     public void ChangeToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
